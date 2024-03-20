65da13c6a95b38b0021d2d31c8471db06b8935a5
CyrilusK <kcyrg17@gmail.com>
2024-03-20 16:55:16 +0300

    added list for passwords

---
+++ android/app/src/main/kotlin/com/kkg/passwd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kkg.passwd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/passwordpage.dart
@@ -0,0 +1,228 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+import 'dbhelper.dart';
+
+class PasswordPage extends StatefulWidget {
+  @override
+  _PasswordPageState createState() => _PasswordPageState();
+}
+
+class _PasswordPageState extends State<PasswordPage> {
+  GlobalKey<FormState> formstate = GlobalKey<FormState>();
+  final dbhelper = Databasehelper.instance;
+  String? type;
+  String? user;
+  String? pass;
+  var allrows = [];
+
+  TextStyle titlestyle = TextStyle(
+    fontSize: 20.0,
+    color: Colors.white,
+  );
+
+  TextStyle subtitlestyle = TextStyle(
+    fontSize: 16.0,
+    color: Colors.white,
+  );
+
+  String? validateempty(_val) {
+    if (_val.isEmpty) {
+      return "Необходимо заполнить поле";
+    } else {
+      return null;
+    }
+  }
+
+  void insertdata() async {
+    Navigator.pop(context);
+    Map<String, dynamic> row = {
+      Databasehelper.columnType: type,
+      Databasehelper.columnUser: user,
+      Databasehelper.columnPass: pass,
+    };
+    final id = await dbhelper.insert(row);
+    print(id);
+    setState(() {});
+  }
+
+  Future<void> queryall() async {
+    allrows = await dbhelper.queryall();
+  }
+
+  void addpassword() {
+    showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+          backgroundColor: Colors.indigo,
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 15.0,
+          ),
+          title: Text("Добавить данные", style: titlestyle),
+          children: <Widget>[
+            Form(
+              key: formstate,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Веб-сайт",
+                      labelStyle: subtitlestyle,
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                    ),
+                    style: titlestyle,
+                    validator: validateempty,
+                    onChanged: (_val) {
+                      type = _val;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5, horizontal: 5),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Логин/е-мейл/номер",
+                        labelStyle: subtitlestyle,
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                      ),
+                      style: titlestyle,
+                      validator: validateempty,
+                      onChanged: (_val) {
+                        user = _val;
+                      },
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Пароль",  labelStyle: subtitlestyle,
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                    ),
+                    style: titlestyle,
+                    validator: validateempty,
+                    onChanged: (_val) {
+                      pass = _val;
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (formstate.currentState!.validate()) {
+                          print("Ready To Enter Data");
+                          insertdata();
+                        }
+                      },
+                      child: Text("Добавить", style: titlestyle),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueAccent),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Сохраненные данные",
+          style: titlestyle
+        ),
+        backgroundColor: Colors.indigo,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addpassword,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.indigo,
+      ),
+      backgroundColor: dark,
+      body: FutureBuilder(
+        builder: (context, AsyncSnapshot<void> snapshot) {
+          if (snapshot.hasData != null) {
+            if (allrows.length == 0) {
+              return Center(
+                child: Text(
+                  "Отсутствуют данные",
+                  style: titlestyle,
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else {
+              return Center(
+                child: Container(
+                  decoration: BoxDecoration(),
+                  width: MediaQuery.of(context).size.width * 0.95,
+                  child: ListView.builder(
+                    itemCount: allrows.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.only(
+                          top: 10.0,
+                        ),
+                        decoration: BoxDecoration(
+                          color: deeppurple,
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 5),
+                                  child: Text(
+                                    allrows[index]['type'],
+                                    textAlign: TextAlign.center,
+                                    style: titlestyle
+                                  ),
+                                ),
+                            ListTile(
+                              leading: Icon(
+                                Icons.facebook,
+                                size: 40.0,
+                                color: Colors.white,
+                              ),
+                              title: Text(
+                                allrows[index]['user'],
+                                style: titlestyle,
+                              ),
+                              subtitle: Text(
+                                allrows[index]['pass'],
+                                style: subtitlestyle,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              );
+            }
+          } else if (snapshot.hasError) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+        future: queryall(),
+      ),
+    );
+  }
+}
